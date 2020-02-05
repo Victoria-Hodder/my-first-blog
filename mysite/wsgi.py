a54e3b60ef132d4512955f47d61e549bb5474920
Victoria-Hodder <morethangrammar@gmail.com>
2020-02-05 11:03:12 +0100

    exported secret_key using dotenv in order to debug PythonAnywhere

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser('~/vrhodder')
+
+load_dotenv(os.path.join(project_folder, '.env'))
